6a6e83ff53abd6053615bd453fe01d8dbb0b16d1
andre-tlima <andre-tlima@hotmail.com>
2021-09-21 14:55:03 +0000

    Adicionando testes para alteração, exclusão e refatoramento

--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -1,22 +1,21 @@
 require 'rails_helper'
-
 RSpec.describe 'Users API', type: :request do
     let!(:user) { create(:user) }
     let(:user_id) { user.id }
-
+    let(:headers) { { "Accept" => " application/vnd.projetofase8.v1" } }
     before{ host! "localhost:3000/api"}
 
     describe "GET user/:id" do
         
         before do
-            headers = { "Accept" => " application/vnd.projetofase8.v1"}
+            #headers = { "Accept" => " application/vnd.projetofase8.v1"}
             get "/users/#{user_id}", params: {}, headers: headers
         end
 
         context "when the user exists" do
             it "returns the user" do
-                user_response = JSON.parse(response.body)
-                expect(user_response["id"]).to eq(user_id)
+                #user_response = JSON.parse(response.body)
+                expect(json_body["id"]).to eq(user_id)
             end
             it "returns status code 200" do
                 expect(response).to have_http_status(200)
@@ -34,7 +33,7 @@ RSpec.describe 'Users API', type: :request do
 
     describe "POST user/" do
         before do
-            headers = { "Accept" => "application/vnd.projetofase8.v1"}
+            #headers = { "Accept" => "application/vnd.projetofase8.v1"}
             post "/users/", params: {user: user_params }, headers: headers
         end
         
@@ -45,8 +44,8 @@ RSpec.describe 'Users API', type: :request do
                 expect(response).to have_http_status(201)
             end
             it "returns json data for the created user" do
-                user_response = JSON.parse(response.body)
-                expect(user_response['email']).to eq(user_params[:email])
+                #user_response = JSON.parse(response.body)
+                expect(json_body['email']).to eq(user_params[:email])
             end
         end
 
@@ -58,9 +57,58 @@ RSpec.describe 'Users API', type: :request do
             end
 
             it "returns the json data for the errors" do
-                user_response = JSON.parse(response.body)
-                expect(user_response).to have_key('errors')
+                #user_response = JSON.parse(response.body)
+                expect(json_body).to have_key('errors')
             end
         end
     end
+
+    describe "PUT user/:id" do
+        before do
+            #headers = {"Accept"=> "application/vnd.projetofase8.v1"}
+            put "/users/#{user_id}", params: {user: user_params}, headers: headers
+        end
+
+        context "when the request params are valid" do
+            let (:user_params){ {email: 'novo@email.com'}}
+
+            it "return status code 200" do
+                expect(response).to have_http_status(200)
+            end
+
+            it "return json data for the update user" do
+                #user_response = JSON.parse(response.body)
+                expect(json_body['email']).to eq(user_params[:email])
+            end
+        end
+
+        context "when the request params are invalid" do
+            let(:user_params){{email: 'email_invalido@'}}
+
+            it "return status code 422" do
+                expect(response).to have_http_status(422)
+            end
+
+            it "returns the json data for the errors" do
+                #user_response = JSON.parse(response.body)
+                expect(json_body).to have_key('errors')
+            end
+        end
+    end
+
+    describe "DELETE user/:id" do
+        before do
+            #headers = {"Accept" => "application/vnd.projetofase8.v1"}
+            delete "/users/#{user_id}", params: {}, headers: headers
+        end
+
+        it "return status code 204" do
+        expect(response).to have_http_status(204)
+        end
+
+        it "removes the user from database" do
+        expect(User.find_by(id: user.id)).to be_nil
+        end
+    end
+
 end
